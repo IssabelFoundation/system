c375662a898cb0e92c25c3ca81b4a253bd238f7a
Nicolas Gudino <asternic@gmail.com>
2021-05-19 10:33:15 -0400

    Update comment

--- modules/shutdown/index.php
+++ modules/shutdown/index.php
@@ -5,6 +5,7 @@
   | Issabel version 0.5                                                  |
   | http://www.issabel.org                                               |
   +----------------------------------------------------------------------+
+  | Copyright (c) 2021 Issabel Foundation                                |
   | Copyright (c) 2006 Palosanto Solutions S. A.                         |
   +----------------------------------------------------------------------+
   | The contents of this file are subject to the General Public License  |
@@ -19,7 +20,8 @@
   +----------------------------------------------------------------------+
   | The Initial Developer of the Original Code is PaloSanto Solutions    |
   +----------------------------------------------------------------------+
-  $Id: index.php,v 1.1.1.1 2007/07/06 21:31:56 gcarrillo Exp $ */
+  $Id: index.php, Wed 19 May 2021 10:33:00 AM EDT, nicolas@issabel.com
+*/
 
 function _moduleContent(&$smarty, $module_name) {
 //include module files
