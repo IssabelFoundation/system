ad9e873439d1157b2a6e6006c9b1239c89dbe16d
Nicolás <nicolas@issabel.com>
2023-10-05 10:20:00 -0400

    Some color sugar to restore

--- modules/backup_restore/restore.php
+++ modules/backup_restore/restore.php
@@ -2,10 +2,10 @@
 /* vim: set expandtab tabstop=4 softtabstop=4 shiftwidth=4:
   Codificación: UTF-8
   +----------------------------------------------------------------------+
-  | Issabel version 4.0.0                                                |
+  | Issabel version 5.0.0                                                |
   | http://www.issabel.org                                               |
   +----------------------------------------------------------------------+
-  | Copyright (c) 2017 Issabel Foundation                                |
+  | Copyright (c) 2023 Issabel Foundation                                |
   +----------------------------------------------------------------------+
   | The contents of this file are subject to the General Public License  |
   | (GPL) Version 2 (the "License"); you may not use this file except in |
@@ -46,7 +46,7 @@ if (isset($_REQUEST['filename'])){
     $filename = $backup_dir."/".$filename;
     if(is_file($filename)) { 
         echo "<div style='font-family: \"Lucida Console\", Monaco, monospace'>";
-	$padSize = ini_get('output_buffering');
+        $padSize = ini_get('output_buffering');
         $dahdi = "";
         $cmd   = "/usr/bin/issabel-helper migration $dahdi $filename";
         $cmd .= " 2>&1 || echo \"err_flag\"";
@@ -58,16 +58,18 @@ if (isset($_REQUEST['filename'])){
                 echo "Error!\n";
                 $error = true;
                 break;
-            }
-	    echo str_pad("$line<br>", $padSize);
+        }
+        if(preg_match("/^Error/i",$line)) {
+            $line='<span style="color:red;">'.$line.'</span>';
+        }
+        echo str_pad("$line<br>", $padSize);
             ob_flush();
             @ flush();
         }
-	pclose($file);
+        pclose($file);
  
         echo "</div>";
-	echo "<script>window.parent.document.getElementById('loader').style.display='none';window.parent.document.getElementById('check').style.display='block';parent.clearInterval(parent.pepe);</script>";
-
+        echo "<script>window.parent.document.getElementById('loader').style.display='none';window.parent.document.getElementById('check').style.display='block';parent.clearInterval(parent.pepe);</script>";
 
     } else {
         echo "$filename is not a file";
